7350b119ae80d88c64fbdf1f94f7f06504041a6b
Evgeniy <mforall29@gmail.com>
2017-03-17 22:53:50 +0300

    onViewTap

--- TinkoffChat/TinkoffChat/MainView.swift
+++ TinkoffChat/TinkoffChat/MainView.swift
@@ -10,13 +10,68 @@ import UIKit
 
 class MainView: UIViewController
 {
+    // MARK: - Outlets
+    
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var aboutUserTV: UITextView!
+    
+    
+    // MARK: - Properties
+    
+    var onViewTapGesture = UITapGestureRecognizer()
+    
+    
+    // MARK: - Life cycle
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        setupLogic()
     }
 
+    
+    // MARK: - Methods
+    
+    func setupLogic()
+    {
+        userNameTF.delegate = self
+        aboutUserTV.delegate = self
+    }
+}
+
+
+// MARK: - Extensions
+
+
+// MARK: UITextFieldDelegate conformation
+
+extension MainView: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+
+// MARK: UITextViewDelegate conformation
+
+extension MainView: UITextViewDelegate
+{
+    func textViewDidBeginEditing(_ textView: UITextView)
+    {
+        onViewTapGesture = UITapGestureRecognizer(target: self, action: #selector(onViewTap))
+        self.view.addGestureRecognizer(onViewTapGesture)
+    }
+    
+    func onViewTap()
+    {
+        self.aboutUserTV.resignFirstResponder()
+        self.view.removeGestureRecognizer(onViewTapGesture)
+    }
 }
 
 extension MainView
