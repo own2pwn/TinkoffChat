4dae19cd822ad04abc66258f738f0cb15914e0da
Evgeniy <mforall29@gmail.com>
2017-03-17 22:05:29 +0300

    layout

---
+++ TinkoffChat/TinkoffChat/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Evgeniy on 17.03.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+extension MainView
+{
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
+}
