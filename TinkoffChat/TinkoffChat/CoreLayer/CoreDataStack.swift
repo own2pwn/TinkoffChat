581040a52580f8e40ddd0d8a085fd6af3e3a98ed
Unknown <mforall29@gmail.com>
2017-04-26 22:15:31 +0300

    CoreDataStack

--- TinkoffChat/TinkoffChat/CoreLayer/CoreDataStack.swift
+++ TinkoffChat/TinkoffChat/CoreLayer/CoreDataStack.swift
@@ -8,3 +8,68 @@
 
 import Foundation
 import CoreData
+
+/*
+ 1. get url of store
+
+ */
+
+protocol ICoreDataStack
+{
+    func retrieveData()
+    func save(data: Data)
+
+}
+
+class CoreDataStack
+{
+    init()
+    {
+        let t = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel!)
+        t.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: <#T##String?#>, at: <#T##URL?#>, options: <#T##[AnyHashable : Any]?#>)
+    }
+
+    // MARK: - Private properties
+
+    // MARK: Lazy
+
+    private lazy var managedObjectModel: NSManagedObjectModel? = {
+        guard let url = Bundle.main.url(forResource: self.managedObjectModelName, withExtension: "momd") else
+        {
+            print("Can't obtain URL for core data storage model")
+            return nil
+        }
+        return NSManagedObjectModel(contentsOf: url)
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        
+        guard let model = self.managedObjectModel else
+        {
+            print("Can't initialize managedObjectModel")
+            return nil
+        }
+        
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        do
+        {
+            try coordinator
+        }
+        
+        catch
+        {
+            
+        }
+        
+        return coordinator
+    }()
+
+    // MARK: Stored
+
+    private let managedObjectModelName = "tfs_chat_model"
+}
+
+/*
+ 1. ViewDidload - fetch, subscribe
+
+ */
