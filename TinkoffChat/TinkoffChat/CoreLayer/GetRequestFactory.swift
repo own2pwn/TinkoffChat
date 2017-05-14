c72e97b305d2d841c8e18a199ed6844d22db2db1
Unknown <mforall29@gmail.com>
2017-05-14 16:05:06 +0300

    RequestSender

---
+++ TinkoffChat/TinkoffChat/CoreLayer/GetRequestFactory.swift
@@ -0,0 +1,17 @@
+//
+//  GetRequestFactory.swift
+//  TinkoffChat
+//
+//  Created by Evgeniy on 14.05.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import Foundation
+
+struct GetRequestFactory
+{
+    struct ImageLoaderGetRequests
+    {
+        
+    }
+}
---
+++ TinkoffChat/TinkoffChat/CoreLayer/RequestSender.swift
@@ -0,0 +1,9 @@
+//
+//  RequestSender.swift
+//  TinkoffChat
+//
+//  Created by Evgeniy on 14.05.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import Foundation
