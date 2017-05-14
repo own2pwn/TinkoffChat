541f51731f0b776df2edc05b228622bb269eab63
Unknown <mforall29@gmail.com>
2017-05-14 16:01:33 +0300

    ImageLoaderService

---
+++ TinkoffChat/TinkoffChat/ServiceLayer/ImageLoaderService.swift
@@ -0,0 +1,19 @@
+//
+//  ImageLoaderService.swift
+//  TinkoffChat
+//
+//  Created by Evgeniy on 14.05.17.
+//  Copyright © 2017 Evgeniy. All rights reserved.
+//
+
+import Foundation
+
+protocol IImageLoaderService
+{
+
+}
+
+class ImageLoaderService: IImageLoaderService
+{
+
+}
